65066922e2201b330756b9600d5bf1faed16955b
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2025-04-04 15:56:46 -0300

    #FasterDragonCurve_Turtle

--- Turtle.py
+++ Turtle.py
@@ -1,5 +1,15 @@
 import turtle
-
+import time
 bob = turtle.Turtle()
+bob.color('green', 'yellow')
+for i in range(4):
+    bob.begin_fill() 
+    for i in range(4): 
+        bob.forward(100)
+        bob.left(90)
+    bob.end_fill()
 
+    bob.penup()
+    bob.forward(30)
+    bob.pendown()
 turtle.done()
