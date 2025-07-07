486624915c2b2a15111942568f9ecfd1e5d68d91
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2025-07-07 00:10:32 -0300

    TTTkinter MenosRepetições WIP(verificar erros nas somas) AdicionarRedeNeural(Futuramente)

--- usingModules/TicTacToe/#TicTacTkinter.py
+++ usingModules/TicTacToe/#TicTacTkinter.py
@@ -8,11 +8,9 @@ soma = [0]*8
 imagemx = ''
 imagemo = ''
 lugares = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-ocupados = lugares[:]
+ocupados = lugares.copy()
 jogador = 1
-pontosx = 0
-pontoso = 0
-empates = 0
+pontuacao = [0]*3
 imagemx = PhotoImage(file='/sysroot/home/user/Documentos/Programaçao/VSCode Python/usingModules/TicTacToe/icons8-cross-90.png')
 imagemo = PhotoImage(file='/sysroot/home/user/Documentos/Programaçao/VSCode Python/usingModules/TicTacToe/icons8-circle-64.png')
 #botões
@@ -39,151 +37,55 @@ def xo(): # X ou O dependendo do jogador.
         return imagemo
 
 def run():
-    global soma, local, ocupados, lugares, pontosx, pontoso, empates, jogador, b
-    # print(ocupados)
-    #while ocupados != []: #Quando apertar um botão
-    try:
-        l = str(local)
+    global soma, local, ocupados, lugares, pontuacao, jogador, b
+    try: #Try Except pode ser removido, foi para debug.
+        l = str(local+1)
         ocupados.pop(ocupados.index(l))
-        l = int(l)
-        lugares[l - 1] = xo()
-        print(xo())
+        l = int(l)-1
+        lugares[l] = xo()
+        # print(xo())
     except Exception as e:
         print(e)
         pass
     else:
-        match l:
-            case 1:
-                if xo() == imagemx:             ######             ######
-                    soma[0] += 1
-                    soma[3] += 1
-                    soma[6] += 1
-                else:
-                    soma[0] -= 1
-                    soma[3] -= 1
-                    soma[6] -= 1
-            case 2:
-                if xo() == imagemx:
-                    soma[0] += 1
-                    soma[5] += 1
-                else:
-                    soma[0] -= 1
-                    soma[5] -= 1
-            case 3:
-                if xo() == imagemx:
-                    soma[0] += 1
-                    soma[5] += 1
-                    soma[7] += 1
-                else:
-                    soma[0] -= 1
-                    soma[5] -= 1
-                    soma[7] -= 1
-            case 4:
-                if xo() == imagemx:
-                    soma[1] += 1
-                    soma[3] += 1
-                else:
-                    soma[1] -= 1
-                    soma[3] -= 1
-            case 5:
-                if xo() == imagemx:
-                    soma[1] += 1
-                    soma[4] += 1
-                    soma[6] += 1
-                    soma[7] += 1
-                else:
-                    soma[1] -= 1
-                    soma[4] -= 1
-                    soma[6] -= 1
-                    soma[7] -= 1
-            case 6:
-                if xo() == imagemx:
-                    soma[2] += 1
-                    soma[5] += 1
-                else:
-                    soma[2] -= 1
-                    soma[5] -= 1
-            case 7:
-                if xo() == imagemx:
-                    soma[2] += 1
-                    soma[3] += 1
-                    soma[7] += 1
-                else:
-                    soma[2] -= 1
-                    soma[3] -= 1
-                    soma[7] -= 1
-            case 8:
-                if xo() == imagemx:
-                    soma[2] += 1
-                    soma[4] += 1
-                else:
-                    soma[2] -= 1
-                    soma[4] -= 1
-            case 9:
-                if xo() == imagemx:
-                    soma[2] += 1
-                    soma[5] += 1
-                    soma[6] += 1
-                else:
-                    soma[2] -= 1
-                    soma[5] -= 1
-                    soma[6] -= 1
-        #tabuleiro()
+        #Cordenadas a serem alteradas NA LISTA DE SOMAS. #A soma é feita com base nas possíveis LINHAS A SEREM FORMADAS, e não nos LUGARES.
+        tabSoma = ({0,3,6},{0,5},{0,5,7},{1,3},{1,4,6,7},{2,5},{2,3,7},{2,4},{2,5,6})#Posso usar o tipo de array mais rápido aqui (set), no lugar das listas :D #E nem precisa do dicionário.
+        for i in tabSoma[l]:
+            if xo() == imagemx:
+                soma[i] += 1
+            else:
+                soma[i] -= 1
+
+        # #tabuleiro()
+        #Verificar vitória
         for s in soma:
-            if s == 3:             ######
+            if s == 3:
                 print("X Ganhou!")
-                pontosx += 1
-                lblx.config(text=f'X: {pontosx}')   
+                pontuacao[0] += 1
+                lblx.config(text=f'X: {pontuacao[0]}')
                 disableAll()
                 break
-            elif s == -3:             ######
+            elif s == -3:
                 print("O Ganhou!")
-                pontoso += 1
-                lblo.config(text=f'O: {pontoso}')
+                pontuacao[1] += 1
+                lblo.config(text=f'O: {pontuacao[1]}')
                 disableAll()
                 break
             elif ocupados == []:
                 print("Deu velha! (Empate!)")
-                empates += 1
-                lble.config(text=f'Empates: {empates}')
+                pontuacao[2] += 1
+                lble.config(text=f'Empates: {pontuacao[2]}')
                 disableAll()
                 break
-        if soma[0] == 3 or soma[0] == -3:
-            b[0].config(bg='#ffcc66')
-            b[1].config(bg='#ffcc66')
-            b[2].config(bg='#ffcc66')
-        if soma[1] == 3 or soma[1] == -3:
-            b[3].config(bg='#ffcc66')
-            b[4].config(bg='#ffcc66')
-            b[5].config(bg='#ffcc66')
-        if soma[2] == 3 or soma[2] == -3:
-            b[6].config(bg='#ffcc66')
-            b[7].config(bg='#ffcc66')
-            b[8].config(bg='#ffcc66')
-        if soma[3] == 3 or soma[3] == -3:
-            b[0].config(bg='#ffcc66')
-            b[3].config(bg='#ffcc66')
-            b[6].config(bg='#ffcc66')
-        if soma[4] == 3 or soma[4] == -3:
-            b[1].config(bg='#ffcc66')
-            b[4].config(bg='#ffcc66')
-            b[7].config(bg='#ffcc66')
-        if soma[5] == 3 or soma[5] == -3:
-            b[2].config(bg='#ffcc66')
-            b[5].config(bg='#ffcc66')
-            b[8].config(bg='#ffcc66')
-        if soma[6] == 3 or soma[6] == -3:
-            b[0].config(bg='#ffcc66')
-            b[4].config(bg='#ffcc66')
-            b[8].config(bg='#ffcc66')
-        if soma[7] == 3 or soma[7] == -3:
-            b[2].config(bg='#ffcc66')
-            b[4].config(bg='#ffcc66')
-            b[6].config(bg='#ffcc66')
+        
+        #Colorir os quadrados vencedores.
+        bColorir = ({0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}) #Tupla com as cordenadas dos possíveis quadrados vencedores.
+        for i in range(8):
+            if soma[i] == 3 or soma[i] == -3: #soma[i] == (3 or -3) ??
+                for c in bColorir[i]:
+                    b[c].config(bg='#ffcc66')
         # print(f"l1:{soma[0]} | l2:{soma[1]} | l3:{soma[2]} | l4:{soma[3]} | l5:{soma[4]} | l6:{soma[5]} | l7:{soma[6]} | l8:{soma[7]}")# Debug
         MudarJogador()
-        print('jogadorMudou')
-
 
 def disableAll():
     global b
@@ -192,19 +94,19 @@ def disableAll():
 
 
 def enableAll():
-    global soma, ocupados, lugares, jogador, pontoso, pontosx, b
+    global soma, ocupados, lugares, jogador, pontuacao, b
     soma = [0] * 8                                                    ######
     ocupados = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    lugares = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    jogador = 1
+    lugares = ocupados.copy()
+    jogador = (sum(pontuacao)%2)+1
     for i in range(9):
         b[i].config(image='',bg='#99ccff',state=ACTIVE,height=5,width=10)
 
 def click(linha,coluna):
     global local, b
-    print(linha, coluna)
-    local = linha*3+coluna+1
-    b[local-1].config(image=xo(),height=83,width=92, state=DISABLED)
+    # print(linha, coluna)
+    local = linha*3+coluna
+    b[local].config(image=xo(),height=83,width=92, state=DISABLED)
     run()
 
 reset = Button(root,font=('mono',30,'bold'),text='New round',command=enableAll).grid(row=5,column=1)
@@ -221,3 +123,5 @@ lblx.grid(row=4,column=0)
 lblo.grid(row=4,column=1)
 lble.grid(row=4,column=2)
 root.mainloop()
+
+##Adicionar uma Rede Neural futuramente.
